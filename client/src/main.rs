26e053a4931c16d23eb4d4d841968a4e69902ebb
Andrew Vitale <vitale232@gmail.com>
2022-09-01 13:24:25 -0400

    keep them modules comin

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,116 +3,21 @@ extern crate dotenv_codegen;
 #[macro_use]
 extern crate serde;
 
+use reqwest::Client;
+use tokio::time::Duration;
+
 mod oauth;
 use oauth::OAuthConfiguration;
 
-use std::fmt::Display;
+mod status;
 
-use reqwest::Client;
-use tokio::time::Duration;
+use crate::status::{get_presence, set_status};
 
 static CLIENT_ID: &str = dotenv!("CLIENT_ID");
 static TENANT_ID: &str = dotenv!("TENANT_ID");
 static PI_IP: &str = dotenv!("PI_IP");
 
 const POLL_AFTER_SECS: u64 = 150;
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-struct Presence {
-    #[serde(rename = "@odata.context")]
-    pub context: String,
-    pub id: String,
-    pub availability: Availability,
-    pub activity: Activity,
-}
-
-impl Presence {
-    fn screen_color(&self) -> String {
-        match self.availability {
-            Availability::Available => "green".into(),
-            Availability::AvailableIdle => "yellow".into(),
-            Availability::Away => "yellow".into(),
-            Availability::BeRightBack => "yellow".into(),
-            Availability::Busy => "red".into(),
-            Availability::BusyIdle => "red".into(),
-            Availability::DoNotDisturb => "red".into(),
-            Availability::Offline => "yellow".into(),
-            Availability::PresenceUnknown => "yellow".into(),
-        }
-    }
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-enum Availability {
-    Available,
-    AvailableIdle,
-    Away,
-    BeRightBack,
-    Busy,
-    BusyIdle,
-    DoNotDisturb,
-    Offline,
-    PresenceUnknown,
-}
-
-impl Display for Availability {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let val = match self {
-            Availability::Available => "Available",
-            Availability::AvailableIdle => "AvailableIdle",
-            Availability::Away => "Away",
-            Availability::BeRightBack => "BeRightBack",
-            Availability::Busy => "Busy",
-            Availability::BusyIdle => "BusyIdle",
-            Availability::DoNotDisturb => "DoNotDisturb",
-            Availability::Offline => "Offline",
-            Availability::PresenceUnknown => "PresenceUnknown",
-        };
-        write!(f, "{}", val)
-    }
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-enum Activity {
-    Available,
-    Away,
-    BeRightBack,
-    Busy,
-    DoNotDisturb,
-    InACall,
-    InAConferenceCall,
-    Inactive,
-    InAMeeting,
-    Offline,
-    OffWork,
-    OutOfOffice,
-    PresenceUnknown,
-    Presenting,
-    UrgentInterruptionsOnly,
-}
-
-impl Display for Activity {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let val = match self {
-            Activity::Available => "Available",
-            Activity::Away => "Away",
-            Activity::BeRightBack => "BeRightBack",
-            Activity::Busy => "Busy",
-            Activity::DoNotDisturb => "DotNotDisturb",
-            Activity::InACall => "InACall",
-            Activity::InAConferenceCall => "InAConferenceCall",
-            Activity::Inactive => "Inactive",
-            Activity::InAMeeting => "InAMeeting",
-            Activity::Offline => "Offline",
-            Activity::OffWork => "OffWork",
-            Activity::OutOfOffice => "OutOfOffice",
-            Activity::PresenceUnknown => "PresenceUnknown",
-            Activity::Presenting => "Presenting",
-            Activity::UrgentInterruptionsOnly => "UrgentInterruptionsOnly",
-        };
-        write!(f, "{}", val)
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -124,30 +29,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     oauth::use_autorefresh(token.clone(), config.clone(), 120);
 
     loop {
-        let presence = client
-            .get("https://graph.microsoft.com/v1.0/me/presence")
-            .header(
-                "Authorization",
-                format!("Bearer {}", token.get_access_token()),
-            )
-            .send()
-            .await?
-            .json::<Presence>()
-            .await?;
+        let presence = get_presence(&client, &token).await?;
         println!("presence: {:#?}", presence);
 
-        let pico_url = format!(
-            "http://{}/{}?top_text=Availability: {}&bottom_text=Activity: {}",
-            PI_IP,
-            presence.screen_color(),
-            presence.availability,
-            presence.activity
-        );
-        let pires = client.get(pico_url).send().await?.text().await?;
-
+        let pires = set_status(&client, &presence, PI_IP).await?;
         println!("Pi Response: {:#?}", pires);
-        println!("Sleeping for {} seconds...", POLL_AFTER_SECS);
 
+        println!("Sleeping for {} seconds...", POLL_AFTER_SECS);
         tokio::time::sleep(Duration::from_secs(POLL_AFTER_SECS)).await;
     }
 
---
+++ client/src/status.rs
@@ -0,0 +1,133 @@
+use std::fmt::Display;
+
+use crate::oauth::SharedAccessToken;
+
+pub async fn get_presence(
+    client: &reqwest::Client,
+    token: &SharedAccessToken,
+) -> Result<Presence, Box<dyn std::error::Error>> {
+    let pres = client
+        .get("https://graph.microsoft.com/v1.0/me/presence")
+        .header(
+            "Authorization",
+            format!("Bearer {}", token.get_access_token()),
+        )
+        .send()
+        .await?
+        .json::<Presence>()
+        .await?;
+    Ok(pres)
+}
+
+pub async fn set_status(
+    client: &reqwest::Client,
+    presence: &Presence,
+    pi_ip_addr: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let pico_url = format!(
+        "http://{}/{}?top_text=Availability: {}&bottom_text=Activity: {}",
+        pi_ip_addr,
+        presence.screen_color(),
+        presence.availability,
+        presence.activity
+    );
+    let pires = client.get(pico_url).send().await?.text().await?;
+    Ok(pires)
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Presence {
+    #[serde(rename = "@odata.context")]
+    pub context: String,
+    pub id: String,
+    pub availability: Availability,
+    pub activity: Activity,
+}
+
+impl Presence {
+    pub fn screen_color(&self) -> String {
+        match self.availability {
+            Availability::Available => "green".into(),
+            Availability::AvailableIdle => "yellow".into(),
+            Availability::Away => "yellow".into(),
+            Availability::BeRightBack => "yellow".into(),
+            Availability::Busy => "red".into(),
+            Availability::BusyIdle => "red".into(),
+            Availability::DoNotDisturb => "red".into(),
+            Availability::Offline => "yellow".into(),
+            Availability::PresenceUnknown => "yellow".into(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum Availability {
+    Available,
+    AvailableIdle,
+    Away,
+    BeRightBack,
+    Busy,
+    BusyIdle,
+    DoNotDisturb,
+    Offline,
+    PresenceUnknown,
+}
+
+impl Display for Availability {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let val = match self {
+            Availability::Available => "Available",
+            Availability::AvailableIdle => "AvailableIdle",
+            Availability::Away => "Away",
+            Availability::BeRightBack => "BeRightBack",
+            Availability::Busy => "Busy",
+            Availability::BusyIdle => "BusyIdle",
+            Availability::DoNotDisturb => "DoNotDisturb",
+            Availability::Offline => "Offline",
+            Availability::PresenceUnknown => "PresenceUnknown",
+        };
+        write!(f, "{}", val)
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum Activity {
+    Available,
+    Away,
+    BeRightBack,
+    Busy,
+    DoNotDisturb,
+    InACall,
+    InAConferenceCall,
+    Inactive,
+    InAMeeting,
+    Offline,
+    OffWork,
+    OutOfOffice,
+    PresenceUnknown,
+    Presenting,
+    UrgentInterruptionsOnly,
+}
+
+impl Display for Activity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let val = match self {
+            Activity::Available => "Available",
+            Activity::Away => "Away",
+            Activity::BeRightBack => "BeRightBack",
+            Activity::Busy => "Busy",
+            Activity::DoNotDisturb => "DotNotDisturb",
+            Activity::InACall => "InACall",
+            Activity::InAConferenceCall => "InAConferenceCall",
+            Activity::Inactive => "Inactive",
+            Activity::InAMeeting => "InAMeeting",
+            Activity::Offline => "Offline",
+            Activity::OffWork => "OffWork",
+            Activity::OutOfOffice => "OutOfOffice",
+            Activity::PresenceUnknown => "PresenceUnknown",
+            Activity::Presenting => "Presenting",
+            Activity::UrgentInterruptionsOnly => "UrgentInterruptionsOnly",
+        };
+        write!(f, "{}", val)
+    }
+}
